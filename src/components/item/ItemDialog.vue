b3b7ee28ff9f966af9067cba8ccb52fd182b8afe
htovey <toveyhe@hotmail.com>
2022-01-30 09:51:29 -0500

    firebase storage

--- src/components/item/ItemDialog.vue
+++ src/components/item/ItemDialog.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
      <v-form
     ref="form"
     v-model="valid"
@@ -46,21 +47,68 @@
     >
         Cancel
     </v-btn>
-
   </v-form>
+  
+  <v-container>
+    <v-layout row>
+      <v-flex class="text-center font-weight-black">
+        <h1>Upload a photo</h1>
+      </v-flex>
+    </v-layout>
+
+   
+    <v-layout row>
+      <v-flex  md6 offset-sm3 >
+       <div>
+         <div >
+           <v-btn @click="click1">choose photo</v-btn>
+           <input type="file" ref="input1"
+            style="display: none"
+            @change="previewImage" accept="image/*" >                
+         </div>
+ 
+       <div v-if="imageData!=null">                     
+          <img class="preview" height="268" width="356" :src="img1">
+       <br>
+       </div>   
+      
+       </div>
+       </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex md6 offset-sm3 class="text-center">
+        <v-text-field
+        solo
+        v-model="caption"
+        label="Caption goes here">
+        </v-text-field>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex class="text-center">
+        <v-btn color="pink" @click="upload">upload</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</div>
 </template>
 <script>
 import { CATEGORIES } from '../../constants/ItemCategories';
+//import {db} from '../../includes/firebase';
+import firebase from 'firebase/app';
 
 export default {
     data() {
       return {
+        caption: '',
+        imageData: {},
+        img1: {},
         valid: true,
-        item: this.$attrs.selectedItem,
+        //item: this.$attrs.selectedItem,
         actionType: this.$parent.actionType,
-        itemNameRef: this.item ? this.item.name : '',
-        itemCategoryRef: this.item ? this.item.category : '',
-        itemDescRef: this.item ? this.item.description : '',
+        itemNameRef: this.$attrs.selectedItem ? this.$attrs.selectedItem.name : '',
+        itemCategoryRef: this.$attrs.selectedItem ? this.$attrs.selectedItem.category : '',
+        itemDescRef: this.$attrs.selectedItem ? this.$attrs.selectedItem.description : '',
         nameRules: [
           v => !!v || 'Item Name is required',
         // v => (v && v.length <= 10) || 'Name must be less than 10 characters',
@@ -83,6 +131,45 @@ export default {
           this.$parent.handleItemSubmit(item);
         },
         
+        upload() {
+          const post = {
+            photo: this.img1,
+            caption: this.caption        
+          }
+          firebase.firestore().ref('PhotoGallery').push(post)
+          .then((response) => {
+            console.log(response)
+          })
+          .catch(err => {
+            console.log(err)
+          })
+        },
+        onUpload(){
+          this.img1=null;
+          const storageRef=firebase.storage().ref(`${this.imageData.name}`).put(this.imageData);
+          storageRef.on(`state_changed`,snapshot=>{
+          this.uploadValue = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
+            }, error=>{console.log(error.message)},
+          ()=>{this.uploadValue=100;
+              storageRef.snapshot.ref.getDownloadURL().then((url)=>{
+                  this.img1 =url;
+                  console.log(this.img1)
+                });
+              }      
+            );
+        },
+
+        click1() {
+          this.$refs.input1.click()   
+        },
+
+        previewImage(event) {
+          this.uploadValue=0;
+          this.img1=null;
+          this.imageData = event.target.files[0];
+          this.onUpload()
+        },
+
         cancel() {
           this.$parent.toggleItem();
         },
